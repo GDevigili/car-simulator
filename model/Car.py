895e073a3dfc49383b5b604f80bfa43e84c66ebc
Gianlucca Devigili <gianluccadevigili@gmail.com>
2022-06-27 12:55:19 -0300

    Add constructor and move methods

--- model/Car.py
+++ model/Car.py
@@ -1,18 +1,43 @@
 import random
-
-from Street import Street
-from Intersection import Intersection
-
+import pygame
 
 class Car:
-    def __init__(self) -> None:
-        """initialize instance of car"""
+    def __init__(self, position, direction: tuple) -> None:
+        """_summary_
+
+        Args:
+            position (tuple[int]): current (x, y) position of the car. (0, 0) is the upper left corner of the screen
+            direction (tuple[int]): describes the direction which the car is going to.
+                It can be up (0, -1), down (0, 1), left (-1, 0) or right (1, 0).
+        """
         self.set_speed()
-            
+        self.position = position
+        self.direction = direction
         pass
 
     def set_speed(self) -> None:
+        self.speed = random.randint(1, 15)
+
+    def change_street(self, intersection):
         pass
 
-    def change_street(self, intersection: Intersection) -> Street:
-        pass
+    def move(self, street) -> None:
+        # add the direction * speed to the current position
+        new_position = (
+            self.position[0] + self.direction[0] * self.speed,
+            self.position[1] + self.direction[1] * self.speed
+        )
+
+        
+
+        self.position = new_position
+
+    def update(self, simulation):
+        self.move(simulation.streets)
+        self.draw(simulation.screen)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0, 0, 255), self.position, 10)
+
+            
+
