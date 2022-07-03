181353cd353da240a3549970aca615def71c8dad
Gianlucca Devigili <gianluccadevigili@gmail.com>
2022-07-03 02:46:33 -0300

    Add functions to insert

--- src/database/db_access.py
+++ src/database/db_access.py
@@ -21,3 +21,87 @@ def connect():
 
 def close_connection(conn):
     conn.close()
+
+
+def insert_scenario(conn, simulation_data:dict):
+    cursor = conn.cursor()
+
+    insert = f""" INSERT INTO scenario (
+                    id,
+                    nbr_streets,
+                    nbr_intersections, 
+                    duration
+                )
+                VALUES ({simulation_data['id']},
+                        {simulation_data['nbr_streets']},
+                        {simulation_data['nbr_intersections']},
+                        {simulation_data['duration']}
+                );
+    """
+
+    cursor.execute(insert)
+    conn.commit()
+
+def insert_car_state(conn, car_state_data:dict):
+    cursor = conn.cursor()
+
+    insert = f""" INSERT INTO car_state (
+                    car_id,
+                    tick_id,
+                    scenario_id,
+                    distance,
+                    current_street_id,
+                    speed
+                )
+                VALUES ({car_state_data['id']},
+                        {car_state_data['tick_counter']},
+                        {car_state_data['simulation_id']},
+                        {car_state_data['distance']},
+                        {car_state_data['street_id']},
+                        {car_state_data['speed']}
+                );
+    """
+
+    cursor.execute(insert)
+    conn.commit()
+
+def insert_tick(conn, tick_data:dict):
+    cursor = conn.cursor()
+
+    insert = f""" INSERT INTO tick (
+                    id,
+                    scenario_id,
+                    seconds_elapsed
+                )
+                VALUES ({tick_data['tick_counter']},
+                        {tick_data['simulation_id']},
+                        {tick_data['time']}
+                );
+    """
+
+    cursor.execute(insert)
+    conn.commit()
+
+if __name__ == "__main__":
+    conn = connect()
+    # insert_scenario(conn, {
+    #     "id": 1,
+    #     "nbr_streets": 2,
+    #     "nbr_intersections": 1,
+    #     "duration": 10
+    # })
+    # insert_tick(conn, {
+    #     "tick_counter": 1,
+    #     "simulation_id": 1,
+    #     "time": 0
+    # })
+    # insert_car_state(conn, {
+    #     "id": 1,
+    #     "tick_counter": 1,
+    #     "simulation_id": 1,
+    #     "distance": 0,
+    #     "street_id": 1,
+    #     "speed": 0
+    # })
+    conn.close()
+    print("Done")
