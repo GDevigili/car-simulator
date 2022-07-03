4676c0992a497349edd7a6429b3ecca05cf296b1
Gianlucca Devigili <gianluccadevigili@gmail.com>
2022-07-03 02:29:25 -0300

    ADD COLUMN car_state.speed

--- src/database/db_create.sql
+++ src/database/db_create.sql
@@ -11,6 +11,7 @@ CREATE TABLE car_state (
     tick_id int NOT NULL,
     scenario_id varchar(36) NOT NULL,
     distance int NOT NULL,
+    speed int NOT NULL,
     current_street_id varchar(36) NOT NULL,
     CONSTRAINT car_state_pk PRIMARY KEY (car_id,tick_id)
 );
